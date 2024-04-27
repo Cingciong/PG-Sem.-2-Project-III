19f525d2c06acb37ff641bb599b33d903a88f9ca
Cingciong <kulakesikula@gmail.com>
2024-04-27 13:44:59 +0200

    adding reverse_dft

--- main.cpp
+++ main.cpp
@@ -35,7 +35,22 @@ std::vector<double> gen_signal(std::string type, double frequency, double time_p
     return signal;
 }
 
+std::vector<double> reverse_dft(const std::vector<std::complex<double>> input) {
+    //X(n) = (1/N) * Σ (from k=0 to N-1) X(k) * e^(j2pikn/N)
+    int N = input.size();
+    std::vector<double> output(N);
 
+    for (int n = 0; n < N; n++) {
+        for (int k = 0; k < N; k++) {
+            double theta = 2 * PI * k * n / N;
+            std::complex<double> exp_part(std::cos(theta), std::sin(theta));
+            output[n] = output[n]+std::real(input[k] * exp_part);
+        }
+        output[n] = output[n] / N;
+    }
+
+    return output;
+}
 std::vector<std::complex<double>> dft(const std::vector<double> input) {
     //X(k) = Σ (from n=0 to N-1) x(n) * e^(-j2pikn/N) it is dft cofficent
     int N = input.size();
@@ -55,5 +70,6 @@ std::vector<std::complex<double>> dft(const std::vector<double> input) {
 
 PYBIND11_MODULE(a1, m) {
     m.def("dft", &dft, py::arg("input"), "A function which calculates the Discrete Fourier Transform");
+    m.def("reverse_dft", &reverse_dft, py::arg("input"), "A function which calculates the Inverse Discrete Fourier Transform");
     m.def("gen_signal", &gen_signal, py::arg("type"), py::arg("frequency"), py::arg("time_period"), "A function which generates a signal wave");
 }
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from build.a1 import *
 
+frequency = 1  # Set the frequency
+time_period = 1  # Set the time period
+
 
 def test_signals():
     frequency = 1  # Set the frequency
@@ -14,13 +17,16 @@ def test_signals():
 
 print("TESTING")
 print("-------------------------------------")
-sin_wave, cos_wave, square_wave, sawtooth_wave = test_signals()
+sin_wave,cos_wave, square_wave, sawtooth_wave =test_signals()
 print("Sin wave: ", sin_wave)
-print("Cos wave: ", cos_wave)
+print("Cos wave: ",cos_wave)
 print("Square wave: ", square_wave)
 print("Sawtooth wave: ", sawtooth_wave)
 print("-------------------------------------")
+print("dft: ")
 print(dft(sin_wave))
+print("reverse dft: ")
+print(reverse_dft(dft(sin_wave)))
 print("TESTING COMPLETE")
 
 
